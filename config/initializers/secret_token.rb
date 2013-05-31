579f3f4fea43b1831bd63aec3e9c1e9c76efca9f
monkeymonkey <teamjrann2@gmail.com>
2013-05-31 16:03:52 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pomegranate::Application.config.secret_token = '5f1a4b63af2126a861f2f1f3f5ae5c6c65604ac951dab90e3c93b544199e3b3b141907807397d765aa4c817749044434d909991427a3af1470b3a81681bb187c'
